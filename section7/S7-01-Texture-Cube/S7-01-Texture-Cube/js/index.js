e54be07b5f2fa3a7a7c8ff16468f65c84d3cb1b5
Recyc <337948531@qq.com>
2019-03-19 11:33:42 +0800

    Initial stage

--- section7/S7-01-Texture-Cube/S7-01-Texture-Cube/js/index.js
+++ section7/S7-01-Texture-Cube/S7-01-Texture-Cube/js/index.js
@@ -1,6 +1,10 @@
 //Setup the global variables
 var camera, scene, renderer, geometry, material, mesh;
 var texture;
+var cubesNum = 10;
+var cubes = [];
+var speed = [];
+
 
 function init() {
 	// Create a scene
@@ -11,15 +15,25 @@ function init() {
 	geometry = new THREE.BoxGeometry( 10, 10, 10 );
 
 	// Load a texture
-	texture = new THREE.TextureLoader().load( "texture.jpg" );
+
+for (let i = 0; i < cubesNum; i++){
+	let randomValue = Math.random()*0.5;
+	speed.push(randomValue);
+};
+
+let randomSelection = Math.round(Math.random()*2);
+texture = new THREE.TextureLoader().load( "texture" + randomSelection + ".jpg");
 
 	// Create a MeshBasicMaterial with a loaded texture
 	material = new THREE.MeshBasicMaterial( { map: texture} );
 
 	// Combine the geometry and material into a mesh
 	mesh = new THREE.Mesh( geometry, material );
+  mesh1 = new THREE.Mesh( geometry, material );
+
 	// Add the mesh to the scene
 	scene.add( mesh );
+  scene.add( mesh1 )
 
 	// Create a camera
 	// 	Set a Field of View (FOV) of 75 degrees
@@ -40,24 +54,32 @@ function init() {
 }
 
 function animate() {
-	// Call the requestAnimationFrame function on the animate function
-	// 	(thus creating an infinite loop)
 	requestAnimationFrame( animate );
-
 	// Rotate the x position of the mesh by 0.03
 	mesh.rotation.x += 0.02;
 	// Rotate the y position of the mesh by 0.02
 	mesh.rotation.y += 0.01;
-
 	//Move the mesh towards the bottom of the screen
 	mesh.position.y -= 0.2;
 
-	//If the mesh passes the bottom of the screen,
-	//make it appear on the top. Also x position is randomized
+	mesh1.rotation.x += 0.04;
+	mesh1.rotation.y += 0.01;
+	mesh1.position.y -= 0.5;
+
+
+
+
 	if (mesh.position.y <- 30){
 		mesh.position.y = 35;
 		mesh.position.x = (Math.random() * -20) +10;
 	}
+
+	if (mesh1.position.y <- 30){
+		mesh1.position.y = 35;
+		mesh1.position.x = (Math.random() * -20) +10;
+	}
+
+
 
 	// Render everything using the created renderer, scene, and camera
 	renderer.render( scene, camera );
